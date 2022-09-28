b04e7d30ef87e79b8c1c10d122a9e8b52bee056e
unknown <rafayfarrukh941@gmail.com>
2022-09-28 14:21:27 +0500

    .

--- src/controllers/tutorials/excel.controller.js
+++ src/controllers/tutorials/excel.controller.js
@@ -3,36 +3,47 @@ const Tutorial = db.tutorials;
 const excelToJson=require('convert-excel-to-json')
 const readXlsxFile = require("read-excel-file/node");
 const excel = require("exceljs");
-var xlsx = require('exceljs');
 
 
-function convertToJSON(array) {
-  var first = array[0].join()
-  var headers = first.split(',');
+var XLSX = require('xlsx');
+var workbook = XLSX.readFile('rafay.xlsx');
+var sheet_name_list = workbook.SheetNames;
+sheet_name_list.forEach(function(y) {
+    var worksheet = workbook.Sheets[y];
+    var headers = {};
+    var data = [];
+    for(z in worksheet) {
+        if(z[0] === '!') continue;
+        //parse out the column, row, and value
+        var tt = 0;
+        for (var i = 0; i < z.length; i++) {
+            if (!isNaN(z[i])) {
+                tt = i;
+                break;
+            }
+        };
+        var col = z.substring(0,tt);
+        var row = parseInt(z.substring(tt));
+        var value = worksheet[z].v;
 
-  var jsonData = [];
-  for ( var i = 1, length = array.length; i < length; i++ )
-  {
+        //store header names
+        if(row == 1 && value) {
+            headers[col] = value;
+            continue;
+        }
 
-    var myRow = array[i].join();
-    var row = myRow.split(',');
-
-    var data = {};
-    for ( var x = 0; x < row.length; x++ )
-    {
-      data[headers[x]] = row[x];
+        if(!data[row]) data[row]={};
+        data[row][headers[col]] = value;
     }
-    jsonData.push(data);
-
-  }
-  return jsonData;
-};
-readXlsxFile('./src/controllers/tutorials/rafay.xlsx', function(err,data) {
-  if(err) throw err;
-  //console.log(jsonDataArray(data));
-  console.log(JSON.stringify(convertToJSON(data)));
-  //console.log(data);
+    //drop those first two rows which are empty
+    data.shift();
+    data.shift();
+    console.log(data);
 });
+
+
+
+
 
 
 
@@ -48,38 +59,38 @@ const upload = async (req, res) => {
 
      
     
-    // readXlsxFile(path).then((rows) => {
-    //   rows.shift();
+    readXlsxFile(path).then((rows) => {
+      rows.shift();
 
-    //   let tutorials = [];
+      let tutorials = [];
 
-    //   rows.forEach((row) => {
-    //     let tutorial = {
-    //       id: row[0],
-    //       data: row[1],
-    //       email: row[2],
+      rows.forEach((row) => {
+        let tutorial = {
+          id: row[0],
+          data: row[1],
+          email: row[2],
          
-    //     };
+        };
   
       
 
        
-    //     tutorials.push(tutorial)
-    //   });
+        tutorials.push(tutorial)
+      });
 
-    //   Tutorial.bulkCreate(tutorials)
-    //     .then(() => {
-    //       res.status(200).send({
-    //         message: "Uploaded the file successfully: " + req.file.originalname,
-    //       });
-    //     })
-    //     .catch((error) => {
-    //       res.status(500).send({ 
-    //         message: "Fail to import data into database!",
-    //         error: error.message,
-    //       });
-    //     });
-    // });
+      Tutorial.bulkCreate(tutorials)
+        .then(() => {
+          res.status(200).send({
+            message: "Uploaded the file successfully: " + req.file.originalname,
+          });
+        })
+        .catch((error) => {
+          res.status(500).send({ 
+            message: "Fail to import data into database!",
+            error: error.message,
+          });
+        });
+    });
   } catch (error) {
     console.log("error jsao "); 
     res.status(500).send({
